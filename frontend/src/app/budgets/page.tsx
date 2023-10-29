81668509d107108f29b43853b7b0852d5f1bd2c9
Markus Priede <mpriede19@gmail.com>
2023-10-29 18:37:06 -0500

    Added some budget styling

--- frontend/src/app/budgets/page.tsx
+++ frontend/src/app/budgets/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import React from 'react';
-import {useState} from "react";
+import { useState } from "react";
 import { FaEdit, FaTrash } from 'react-icons/fa';
+import Switch from 'react-switch'
 
 interface IBudgetItem {
   id: number;
@@ -10,99 +11,99 @@ interface IBudgetItem {
   assigned: string,
   amount: string,
   available: string,
-  enable: boolean  
+  enable: boolean
 }
 
 const Budgets: React.FC = () => {
-    const [budgetItems, setBudgetItems] = useState<IBudgetItem[]>([
-      {
-        id: 1,
-        category: "Household",
-        assigned: "$650.00",
-        amount: "$500.00",
-        available: "$52.00",
-        enable: true
-      },
-      {
-        id: 2,
-        category: "Dining",
-        assigned: "$650.00",
-        amount: "$500.00",
-        available: "$52.00",
-        enable: true
-      },
-      {
-        id: 3,
-        category: "Work",
-        assigned: "$650.00",
-        amount: "$500.00",
-        available: "$52.00",
-        enable: true
-      },
-      
-    ]);
+  const [budgetItems, setBudgetItems] = useState<IBudgetItem[]>([
+    {
+      id: 1,
+      category: "Household",
+      assigned: "$650.00",
+      amount: "$500.00",
+      available: "$52.00",
+      enable: true
+    },
+    {
+      id: 2,
+      category: "Dining",
+      assigned: "$650.00",
+      amount: "$500.00",
+      available: "$52.00",
+      enable: true
+    },
+    {
+      id: 3,
+      category: "Work",
+      assigned: "$650.00",
+      amount: "$500.00",
+      available: "$52.00",
+      enable: true
+    },
 
-    const toggleEnable = (id: number) => {
-        setBudgetItems(prevItems =>
-          prevItems.map(item =>
-            item.id === id ? { ...item, enable: !item.enable } : item
-          )
-        );
-      };
-    
-      const deleteItem = (id: number) => {
-        setBudgetItems(prevItems => prevItems.filter(item => item.id !== id));
-      };
+  ]);
 
-      //make the rows fixed till user presses edit icon
-      const [editingId, setEditingId] = useState<number | null>(null);
+  const toggleEnable = (id: number) => {
+    setBudgetItems(prevItems =>
+      prevItems.map(item =>
+        item.id === id ? { ...item, enable: !item.enable } : item
+      )
+    );
+  };
 
-      const editItem = (id: number) => {
-        setEditingId(id);
+  const deleteItem = (id: number) => {
+    setBudgetItems(prevItems => prevItems.filter(item => item.id !== id));
+  };
+
+  //make the rows fixed till user presses edit icon
+  const [editingId, setEditingId] = useState<number | null>(null);
+
+  const editItem = (id: number) => {
+    setEditingId(id);
+  };
+
+  const addBudget = () => {
+    const newBudget: IBudgetItem = {
+      id: Date.now(),
+      category: "New Category",
+      assigned: "$0.00",
+      amount: "$0.00",
+      available: "$0.00",
+      enable: true
     };
 
-      const addBudget = () => {
-        const newBudget: IBudgetItem = {
-            id: Date.now(), 
-            category: "New Category",
-            assigned: "$0.00",
-            amount: "$0.00",
-            available: "$0.00",
-            enable: true
-        };
-
-        setBudgetItems(prevItems => [...prevItems, newBudget]);
-        setEditingId(newBudget.id);
-    };
-    //make componenets editable
-    const updateField = (id: number, field: keyof IBudgetItem, value: string | boolean) => {
-      setBudgetItems(prevItems => 
-          prevItems.map(item => 
-              item.id === id ? { ...item, [field]: value } : item
-          )
-      );
+    setBudgetItems(prevItems => [...prevItems, newBudget]);
+    setEditingId(newBudget.id);
+  };
+  //make componenets editable
+  const updateField = (id: number, field: keyof IBudgetItem, value: string | boolean) => {
+    setBudgetItems(prevItems =>
+      prevItems.map(item =>
+        item.id === id ? { ...item, [field]: value } : item
+      )
+    );
   };
   //format values upto 2 decimal places and prefix with dollar sign
   const formatCurrency = (value: string) => {
     const numValue = parseFloat(value);
     if (isNaN(numValue)) return "$0.00";
     return `$${numValue.toFixed(2)}`;
-};
- 
+  };
+
   return (
     <div className="p-6">
       <h1 className="text-4xl font-bold mb-4">Budget/Project</h1>
       <div className="flex items-center space-x-4">
         <p className="text-gray-400">Current Duration:</p>
         <p className="text-gray-600">08/15/2023 - 09/14/2023</p>
-        <button className="bg-blue-500 text-white rounded-full px-8 py-2 space-x-4">
+        <button className="bg-blue-100 text-white rounded-full px-8 py-2 space-x-4">
           <span className="flex-1 bg-white w-1/2 h-full rounded-l-full"></span>
           <span className="flex-1 bg-white w-1/2 h-full rounded-r-full"></span>
         </button>
       </div>
 
-     <table className="w-full text-left shadow-md text-xs font-sans border-collapse mt-3 rounded-md">
-        <thead className="bg-blue-300">
+      <table className="w-full text-left shadow-md text-base font-sans border-collapse mt-3 rounded-md">
+        <thead className="bg-blue-100">
           <tr>
             <th className="py-3 pl-3 w-1/6">Category</th>
             <th className="py-3 pl-3 w-1/6">Assigned</th>
@@ -114,88 +115,89 @@ const Budgets: React.FC = () => {
         </thead>
         <tbody>
 
-    {budgetItems.map(item => (
-    <tr key={item.id} 
-    className={`border-t-2 ${editingId === item.id ? 'bg-blue-300' : ''}`}
->
-        <td className="py-2 pl-3">
-            <input 
-                type="text" 
-                value={item.category}
-                onChange={(e) => updateField(item.id, 'category', e.target.value)} 
-                readOnly={editingId !== item.id}
-                placeholder="New Category"
-                
+          {budgetItems.map(item => (
+            <tr key={item.id}
+              className={`border-t-2 ${editingId === item.id ? 'bg-blue-300' : ''} text-base`}
+            >
+              <td className="py-2 pl-3">
+                <input
+                  type="text"
+                  value={item.category}
+                  onChange={(e) => updateField(item.id, 'category', e.target.value)}
+                  readOnly={editingId !== item.id}
+                  placeholder="New Category"
+                  className="rounded"
+                />
+              </td>
+              <td className="py-2 pl-3">
+                <input
+                  type="text"
+                  value={item.assigned}
+                  onBlur={(e) => updateField(item.id, 'assigned', formatCurrency(e.target.value))}
+                  onChange={(e) => updateField(item.id, 'assigned', e.target.value)}
+                  readOnly={editingId !== item.id}
+                  placeholder="$0.00"
+                  className="rounded"
 
-            />
-        </td>
-        <td className="py-2 pl-3">
-            <input 
-               type="text" 
-               value={item.assigned}
-               onBlur={(e) => updateField(item.id, 'assigned', formatCurrency(e.target.value))}
-               onChange={(e) => updateField(item.id, 'assigned', e.target.value)}
-               readOnly={editingId !== item.id}
-               placeholder="$0.00"
-               
-            />
-        </td>
-        <td className="py-2 pl-3">
-            <input 
-                type="text" 
-                value={item.amount}
-                onBlur={(e) => updateField(item.id, 'amount', formatCurrency(e.target.value))}
-                onChange={(e) => updateField(item.id, 'amount', e.target.value)}
-                readOnly={editingId !== item.id}
-                placeholder="$0.00"
-                
-            />
-        </td>
-        <td className="py-2 pl-3">
-            <input 
-               type="text" 
-               value={item.available}
-               onBlur={(e) => updateField(item.id, 'available', formatCurrency(e.target.value))}
-               onChange={(e) => updateField(item.id, 'available', e.target.value)}
-               readOnly={editingId !== item.id}
-               placeholder="$0.00"
-              
-            />
-        </td>
-        <td className="py-2 pl-3">
-            <input 
-                type="checkbox" 
-                checked={item.enable} 
-                onChange={() => toggleEnable(item.id)} 
-            />
-        </td>
-        <td className="py-2 pl-3 flex justify-around">
-    {editingId === item.id ? (
-        <>
-            <span onClick={() => setEditingId(null)}>Save</span> 
-            <span onClick={() => setEditingId(null)}>Cancel</span>
-        </>
-    ) : (
-        <>
-            <FaEdit 
-                style={{ cursor: 'pointer' }} 
-                onClick={() => editItem(item.id)}
-            />
-            <FaTrash 
-                style={{ cursor: 'pointer' }} 
-                onClick={() => deleteItem(item.id)} 
-            />
-        </>
-    )}
-</td>
-    </tr>
-))}
+                />
+              </td>
+              <td className="py-2 pl-3">
+                <input
+                  type="text"
+                  value={item.amount}
+                  onBlur={(e) => updateField(item.id, 'amount', formatCurrency(e.target.value))}
+                  onChange={(e) => updateField(item.id, 'amount', e.target.value)}
+                  readOnly={editingId !== item.id}
+                  placeholder="$0.00"
+                  className="rounded"
+                />
+              </td>
+              <td className="py-2 pl-3">
+                <input
+                  type="text"
+                  value={item.available}
+                  onBlur={(e) => updateField(item.id, 'available', formatCurrency(e.target.value))}
+                  onChange={(e) => updateField(item.id, 'available', e.target.value)}
+                  readOnly={editingId !== item.id}
+                  placeholder="$0.00"
+                  className="rounded "
+
+                />
+              </td>
+              <td className="py-2 pl-3">
+                <Switch
+                  checked={item.enable}
+                  onChange={() => toggleEnable(item.id)}
+                  height={20}
+                  width={40}
+                />
+              </td>
+              <td className="py-2 pl-3 flex items-center space-x-2">
+                <div className="flex space-x-2">
+                  <FaEdit
+                    style={{ cursor: 'pointer', fontSize: '20px' }} 
+                    onClick={() => editItem(item.id)}
+                  />
+                  <FaTrash
+                    style={{ cursor: 'pointer', fontSize: '20px' }} 
+                    onClick={() => deleteItem(item.id)}
+                  />
+                </div>
+                {editingId === item.id &&
+                  <>
+                    <span onClick={() => setEditingId(null)} style={{cursor: 'pointer'}}>Save</span>
+                    <span onClick={() => setEditingId(null)} style={{cursor: 'pointer'}}>Cancel</span>
+                  </>
+                }
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className="flex justify-center">
-      <button className="text-blue-700 bg-blue-100 rounded-full px-4 py-2 mt-4" onClick={addBudget}>
-      Add Budget Item 
-      </button>
+        <button className="text-blue-700 bg-blue-100 rounded-full px-4 py-2 mt-4" onClick={addBudget}>
+          Add Budget
+        </button>
       </div>
     </div>
   );
@@ -204,4 +206,3 @@ const Budgets: React.FC = () => {
 
 export default Budgets;
 
-  
