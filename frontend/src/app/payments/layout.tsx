1be13bdb77269ff96d30066d13d6b81b2586cf86
spandanjadhav <sujadhav@wisc.edu>
2023-11-10 08:59:41 -0600

    some subscription changes

--- frontend/src/app/payments/layout.tsx
+++ frontend/src/app/payments/layout.tsx
@@ -11,16 +11,9 @@ export default function PaymentsLayout({ children }: { children: React.ReactNode
           <a className="py-3 pl-2">Previous Billing</a>
           <a className="py-3 pl-2">Financial Summary</a>
           <a className="py-3 pl-2">Custom Date Range</a>
-          <a className="py-3 pl-2" href="/payments/subscriptions">Subscription Management</a>
+          <a className="py-3 pl-2 border-l-2 border-blue-600 bg-blue-100" href="/payments/subscriptions">Subscription Management</a>
         </div>
-
         <div className="flex grow-0 shrink flex-col pt-3 gap-8">
-          <div className="flex gap-16">
-            <Card title="Statement Balance" line1="$100.50" line2="Aug 15 - Sep 14" buttonText="View Transactions" />
-            <Card title="Payment Due on" line1="September 30" line2="Minimum Payment Due $20.00" buttonText="Make Payment" />
-            <Card title="Payment Due on" line1="September 30" line2="Minimum Payment Due $20.00" buttonText="Make Payment" />
-          </div>
-
           { children }
         </div>
 
--- frontend/src/app/payments/page.tsx
+++ frontend/src/app/payments/page.tsx
@@ -3,10 +3,17 @@ import React from 'react'
 import Card from "../components/Card";
 import Transactions from '../components/Transactions';
 
-const Payments: React.FC = () => { 
+const Payments: React.FC = () => {
   return (
-    <Transactions />
-  )
-}
+    <div className="flex flex-col gap-16">
+      <div className="flex gap-16">
+        <Card title="Statement Balance" line1="$100.50" line2="Aug 15 - Sep 14" buttonText="View Transactions" />
+        <Card title="Payment Due on" line1="September 30" line2="Minimum Payment Due $20.00" buttonText="Make Payment" />
+        <Card title="Payment Due on" line1="September 30" line2="Minimum Payment Due $20.00" buttonText="Make Payment" />
+      </div>
+      <Transactions />
+    </div>
+  );
+};
 
 export default Payments;
