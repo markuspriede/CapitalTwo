ac56456eee9ec70ad5ce8d00841f4629444c5edb
spandanjadhav <sujadhav@wisc.edu>
2023-11-17 23:25:49 -0600

    subscription styling

--- frontend/src/app/payments/subscriptions/page.tsx
+++ frontend/src/app/payments/subscriptions/page.tsx
@@ -8,6 +8,8 @@ const SubscriptionsPage: React.FC = () => {
     subTitle: 'Calculated to today',
     subPlan: '',
     showLogo: false,
+    customHeightCard0: true,
+    customWidthCard0: true,
   };
 
   const card1 = {
@@ -17,6 +19,8 @@ const SubscriptionsPage: React.FC = () => {
     subPlan: 'Monthly',
     comingUp: 'Coming Up', 
     showLogo: false,  
+    customHeightCard1: true,
+    customWidthCard1: true,
   };
 
   const card2 = {
@@ -47,33 +51,47 @@ const SubscriptionsPage: React.FC = () => {
     subPlan: 'Monthly',
   };
 
+  const card6 = {
+    title: 'YouTube Premium',
+    price: '$10.00',
+    subTitle: 'Student Premium Plan',
+    subPlan: 'Monthly',
+  };
+
+  const card7 = {
+    title: 'YouTube Premium',
+    price: '$10.00',
+    subTitle: 'Student Premium Plan',
+    subPlan: 'Monthly',
+  };
 
   return (
     <div className="p-6">
-      <div className="mt-4">
+    <div className="mt-4">
       <div className="flex flex-wrap justify-start gap-20 mt-4 mb-4">
-          <CustomCard {...card0} center />
-          <CustomCard {...card1} />
-          </div>
-        <div className="flex justify-around mb-6 mr-40">
-          <button className="bg-blue-200 text-blue-900 font-bold px-4 py-2 rounded-md w-80">
-            View Calendar
-          </button>
-        </div>
+        <div><CustomCard {...card0} center /></div>
+        <div><CustomCard {...card1} /></div>
       </div>
-
+      <div className="flex justify-start mb-4 ml-96">
+        <button className="bg-blue-200 text-blue-900 font-bold px-4 py-2 rounded-md w-96">
+        View Calendar
+        </button>
+      </div>
+      </div>
 
       <div className="border-t border-gray-300 mt-10"></div>
       <h2 className="text-2xl font-bold mt-10">Subscription Management</h2>
-      
+
       <div className="flex flex-wrap justify-start mt-10">
         <div className="flex flex-wrap justify-start gap-20 w-full">
           <CustomCard {...card2} />
           <CustomCard {...card3} />
+          <CustomCard {...card4} />
         </div>
         <div className="flex flex-wrap justify-start gap-20 w-full mt-6">
-          <CustomCard {...card4} />
           <CustomCard {...card5} />
+          <CustomCard {...card6} />
+          <CustomCard {...card7} />
         </div>
       </div>
     </div>
