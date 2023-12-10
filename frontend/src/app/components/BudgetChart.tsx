7ff1b7efa0b3e49d33356746b8e0030e8c67fbdb
simar-singh-1 <ssingh287@wisc.edu>
2023-12-09 18:29:08 -0600

    Added Budget Chart
    Added chart to display budget information in a visual way based on the wireframes

---
+++ frontend/src/app/components/BudgetChart.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+interface IBudgetChartProps {
+  refresh: Number,
+  setRefresh: any
+}
+
+const BudgetChart = (props: IBudgetChartProps) => {
+  const [budgets, setBudgets] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch(`http://3.84.112.106/budget`).then((res) => res.json()).then((data) => {
+      setBudgets(data);
+    }).catch((error) => console.error('Error fetching data:', error));
+  }, [props.refresh]);
+
+  return <div className="w-full">
+    {
+      budgets.map((budget, index) => {
+        if (index > 1) {
+          return <div key={index}></div>;
+        }
+
+        const over = budget.amount_spent > budget.budget_amount;
+        const percentage = budget.amount_spent < budget.budget_amount ? (budget.amount_spent / budget.budget_amount) * 100 : 100;
+
+        return <div className="w-full bg-grey-light my-2" key={index}>
+        <div className="flex flex-nowrap justify-between mb-1 content-end">
+          <h2 className="text-md text-slate-500 align-text-bottom flex-nowrap">{ budget.category_name }</h2>
+          <div className={`${over ? "text-red-600" : "text-green-600"} text-end`}>
+            <p className="text-xs">{ over ? "Over budget" : "Remaining" }</p>
+            <p>${ over ? (budget.amount_spent - budget.budget_amount).toFixed(2) : (budget.budget_amount - budget.amount_spent).toFixed(2) }</p>
+          </div>
+        </div>
+        <div className="w-full bg-gray-300 rounded-full h-2">
+          <div className="bg-blue-700 h-2 rounded-full" style={{ width: `${percentage}%` }}></div>
+        </div>
+        <div className="flex justify-end mt-1">
+          <p className="text-sm text-slate-500">{`${budget.amount_spent} / ${budget.budget_amount}`}</p>
+        </div>
+      </div>
+      })
+    }
+  </div>
+}
+
+export default BudgetChart;
--- frontend/src/app/payments/page.tsx
+++ frontend/src/app/payments/page.tsx
@@ -1,17 +1,19 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import Card from "../components/Card";
 import Transactions from '../components/Transactions';
 
 const Payments: React.FC = () => {
+  const [refresh, setRefresh] = useState(0);
+
   return (
     <div className="flex flex-col gap-16">
       <div className="flex gap-16">
-        <Card title="Statement Balance" line1="$100.50" line2="Aug 15 - Sep 14" buttonText="View Transactions" />
-        <Card title="Payment Due on" line1="September 30" line2="Minimum Payment Due $20.00" buttonText="Make Payment" />
-        <Card title="Payment Due on" line1="September 30" line2="Minimum Payment Due $20.00" buttonText="Make Payment" />
+        <Card title="Statement Balance" line1="$100.50" line2="Dec 15 - Jan 14" buttonText="View Transactions" />
+        <Card title="Payment Due on" line1="December 30" line2="Minimum Payment Due $20.00" buttonText="Make Payment" />
+        <Card title="Total Balance" line1="$100.50" line2="Balance Details" buttonText="View Balance" />
       </div>
-      <Transactions />
+      <Transactions refresh={refresh} setRefresh={setRefresh} />
     </div>
   );
 };
